b3f55e7d6e5889c696f775e8382a1b3916328442
Horiaki_hori@asobimo.com <aki_hori@asobimo.com>
2018-11-20 14:01:14 +0900

    [add]unity 1week 事はじめ

---
+++ one_week_ten/Assets/scripts/MapBlock.cs
@@ -0,0 +1,57 @@
+﻿//===================================================================================================
+/// <summary>
+/// 概要：このクラスは、マップブロック単位での、構造体的な扱いのクラスです。
+/// 基本的にこのクラスは入れられたマップ情報に対して参照をされるだけの存在です。
+///
+///
+///
+/// MapBlock.cs
+/// </summary>
+/// <Author>
+/// 堀 明博 aki_hori@asobimo.com
+///                                         後から他人が大幅な改変や流用を行った場合は2行目以降に記載
+/// </Author>
+//===================================================================================================
+
+using UnityEngine;
+using System.Collections;
+
+public class MapBlock
+{
+	/// <summary>
+	/// 表示を行うかどうかのもの
+	/// </summary>
+	private bool is_display;
+
+	private GameObject created_prefab;
+
+	//一時的なコンストラクタ
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="_display"></param>
+	/// <param name="_prefab"></param>
+	/// <param name="_setting_pos"></param>
+	public MapBlock(bool _display,GameObject _prefab,Vector3 _setting_pos)
+	{
+		if(_display)
+		{
+			created_prefab = GameObject.Instantiate(_prefab);
+			created_prefab.transform.position = _setting_pos;
+		}
+
+
+	}
+
+
+	/// <summary>
+	/// ここに関連している内容の中身を消す
+	/// </summary>
+	public void BlockMapDestroy()
+	{
+		if(null != created_prefab)
+		{
+			GameObject.Destroy(created_prefab);
+		}
+	}
+}
---
+++ one_week_ten/Assets/scripts/MapField.cs
@@ -0,0 +1,34 @@
+﻿//===================================================================================================
+/// <summary>
+/// 概要：このクラスは、MapBlockを管理するためのものです。
+/// このクラスはMappingCreateから作成され、情報の取得を行います。
+///
+///
+///
+/// MapField.cs
+/// </summary>
+/// <Author>
+/// 堀 明博 aki_hori@asobimo.com
+///                                         後から他人が大幅な改変や流用を行った場合は2行目以降に記載
+/// </Author>
+//===================================================================================================
+
+using UnityEngine;
+using System.Collections;
+
+public class MapField
+{
+
+
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="_mapping_map">作成するマップ情報</param>
+	/// <param name="_width">横に配置されるブロック数</param>
+	/// <param name="_height">縦に配置されるブロック数</param>
+	public MapField(bool[] _mapping_map , int _width,int _height)
+	{
+
+	}
+}
